cf4066f421c11df528bb9b55b90f62a15a2fa0cd
paulsh3rin <paulsherin22@gmail.com>
2024-08-09 17:20:42 -0400

    Edited Fetch URL

--- static/js/script.js
+++ static/js/script.js
@@ -6,7 +6,7 @@ document.getElementById('summarizeButton').addEventListener('click', async () =>
     }
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/summarize', {
+        const response = await fetch('/summarize', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
